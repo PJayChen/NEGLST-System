c2333937831226ac8a1155693dfd165dbc981e58
PJayChen <n26021539@mail.ncku.edu.tw>
2015-01-21 11:24:32 +0800

    Enable USART2 for GPS

--- src/hw_conf.c
+++ src/hw_conf.c
@@ -1,14 +1,5 @@
 #include "hw_conf.h"
 
-
-void prvSetupHardware(void)
-{
-	/* Set the Vector Table base address at 0x08000000 */
-	NVIC_SetVectorTable( NVIC_VectTab_FLASH, 0x0 );
-	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 ); //only preempt priority
- 	USART1_Configuration();
-}
-
 //USART1 is use for Shell(Command Line)
 void USART1_Configuration(void)
 {
@@ -65,4 +56,73 @@ void USART1_Configuration(void)
 	NVIC_Init(&NVIC_InitStructure);
 
 	USART_Cmd(USART1, ENABLE);
+}
+
+//USART2 is used for communicate with GPS
+void USART2_Configuration(void)
+{
+	/* --------------------------- System Clocks Configuration -----------------*/
+	/* USART2 clock enable */
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+	/* GPIOA clock enable */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);	
+	
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+    /*-------------------------- GPIO Configuration ----------------------------*/
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_2 | GPIO_Pin_3;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /* Connect USART pins to AF */
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource2, GPIO_AF_USART2); // USART2_TX
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource3, GPIO_AF_USART2); // USART2_RX
+  	
+	/*-------------------------- USART Configuration ----------------------------*/
+	//Structure With Data For USART Configuration
+  	USART_InitTypeDef USART_InitStructure;
+  
+	//USART Parameters
+	USART_InitStructure.USART_BaudRate = 9600;
+	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+	USART_InitStructure.USART_StopBits = USART_StopBits_1;
+	USART_InitStructure.USART_Parity = USART_Parity_No ;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx ;
+
+	//Configuring And Enabling USART2
+	USART_Init(USART2, &USART_InitStructure);
+
+#if 0
+	
+	/* Enable transmit and receive interrupts for the USART2. */
+	USART_ITConfig(USART2, USART_IT_TXE, ENABLE);
+	USART_ITConfig(USART2, USART_IT_RXNE, ENABLE);  
+
+	/*--------------------------- Enable USART Interrupt -------------------------*/
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	USART_ClearFlag(USART2, USART_FLAG_TC);
+	
+	/* Enable the USART2 IRQ in the NVIC module (so that the USART2 interrupt
+	* handler is enabled). */
+	NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = configMAX_SYSCALL_INTERRUPT_PRIORITY + 0x20;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+	NVIC_Init(&NVIC_InitStructure);
+#endif
+	USART_Cmd(USART2, ENABLE);
+}
+
+void prvSetupHardware(void)
+{
+	/* Set the Vector Table base address at 0x08000000 */
+	NVIC_SetVectorTable( NVIC_VectTab_FLASH, 0x0 );
+	NVIC_PriorityGroupConfig( NVIC_PriorityGroup_4 ); //only preempt priority
+ 	USART1_Configuration();
+ 	USART2_Configuration();
 }
--- src/hw_it.c
+++ src/hw_it.c
@@ -10,12 +10,15 @@
 extern xQueueHandle xQueueUART1Recvie;
 extern SemaphoreHandle_t xSemUSART1send;
 
+extern xQueueHandle xQueueUSART2Recvie;
+extern SemaphoreHandle_t xSemUSART2send;
+
 /* Queue structure used for passing characters. */
 typedef struct {
         char ch;
 } serial_ch_msg;
 
-/* IRQ handler to handle USART2 interruptss (both transmit and receive
+/* IRQ handler to handle USART1 interruptss (both transmit and receive
 * interrupts). */
 void USART1_IRQHandler()
 {
@@ -51,3 +54,31 @@ void USART1_IRQHandler()
     }
 
 }
+
+/* IRQ handler to handle USART2 interruptss (both transmit and receive
+* interrupts). */
+void USART2_IRQHandler()
+{
+    static signed portBASE_TYPE xHigherPriorityTaskWoken;
+    serial_ch_msg rx_msg;
+
+    /* If this interrupt is for a transmit... */
+    if (USART_GetITStatus(USART2, USART_IT_TXE) != RESET) {
+            
+        /* Diables the transmit interrupt. */
+        //USART_ITConfig(USART2, USART_IT_TXE, DISABLE);
+        //xSemaphoreGiveFromISR(xSemUSART2send, &xHigherPriorityTaskWoken); 
+
+    /* If this interrupt is for a receive... */
+    }else if (USART_GetITStatus(USART2, USART_IT_RXNE) != RESET) {
+            /* Receive the byte from the buffer. */
+            rx_msg.ch = USART_ReceiveData(USART2);
+
+            /* Queue the received byte. */
+            xQueueSendToBackFromISR(xQueueUSART2Recvie, &rx_msg, &xHigherPriorityTaskWoken);  
+    }
+    if (xHigherPriorityTaskWoken) {
+            taskYIELD();
+    }
+}
+
