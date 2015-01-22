4daaa972cd7bdceb1eed1d9b730bf3bf3aee9259
PJayChen <n26021539@mail.ncku.edu.tw>
2015-01-22 12:04:34 +0800

    add ifndef in shell.h

---
+++ inc/shell.h
@@ -0,0 +1,27 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* Standard includes. */
+#include "string.h"
+#include "stdio.h"
+
+/* FreeRTOS includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+
+/* Example includes. */
+#include "FreeRTOS_CLI.h"
+
+/* print to USART includes. */
+#include "myio.h"
+
+
+/*-----------------------------------------------------------*/
+
+/*
+ * The task that implements the command console processing.
+ */
+void prvUARTCommandConsoleTask( void *pvParameters );
+void vUARTCommandConsoleStart( uint16_t usStackSize, UBaseType_t uxPriority );
+
+#endif
